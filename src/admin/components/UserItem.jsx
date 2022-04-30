53e7cc7ba8a5d68ddf74ddecda952dd396ae51a7
garman000 <garman000@gmail.com>
2022-04-30 22:35:09 +0300

    changes to style

--- src/admin/components/UserItem.jsx
+++ src/admin/components/UserItem.jsx
@@ -8,25 +8,23 @@ const UserItem = (props) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-          {/* <Card> */}
-          <Link to={`/${props.id}/mypets`}>
-      {/* <div className="test1"> */}
-        <div className="user-item__image">
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.firstname} {props.lastname}</h2>
-          {/* <h3>Age: {props.age}</h3> */}
-          <h3>
-            {props.petCount} {props.petCount === 1 ? "Pet" : "Pets"}
-          </h3>
+        <Link to={`/${props.id}/mypets`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
           </div>
-        {/* </div> */}
+          <div className="user-item__info">
+            <h2>
+              {props.firstname} {props.lastname}
+            </h2>
+
+            <h3>
+              {props.petCount} {props.petCount === 1 ? "Pet" : "Pets"}
+            </h3>
+          </div>
         </Link>
-        {/* </Card> */}
       </div>
     </li>
   );
-}
+};
 
 export default UserItem;
